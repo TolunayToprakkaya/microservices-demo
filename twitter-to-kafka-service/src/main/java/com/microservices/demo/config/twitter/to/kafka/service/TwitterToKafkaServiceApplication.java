c01ca8a3b2f69dd087b71d336b67e897b6db4102
TolunayToprakkaya <tolunaytoprakkaya@gmail.com>
2023-12-16 17:36:06 +0300

    log change

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/config/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/config/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -31,7 +31,7 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        LOG.info("App starts...");
+        LOG.info("App starts....");
         LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
         streamRunner.start();
